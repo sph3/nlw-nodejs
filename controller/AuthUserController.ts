2541769e9515c0c723a08f16dc96956d3c3a75d8
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 16:35:17 -0300

    É isso

--- controller/AuthUserController.ts
+++ controller/AuthUserController.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from 'express';
+import { Request, response, Response } from 'express';
 import { AuthUserService } from '../service/AuthUserService';
 
 export class AuthUserController {
   async handle(req: Request, res: Response) {
+    const { code } = req.body;
     const service = new AuthUserService();
-    // service.execute();
+    const result = await service.execute(code);
+    return response.json(result);
   }
 }
