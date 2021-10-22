10f52ece8d0ac0bb9256afbda636f56e7d81d090
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 17:22:50 -0300

    Access token

--- service/AuthUserService.ts
+++ service/AuthUserService.ts
@@ -4,25 +4,31 @@ import { response } from 'express';
 /*
  * Receber code(string)
  * Recuperar o access_token no Github
+ * Recuperar infos do user no Github
  * Verificar se o usuário existe na DB
  * Se sim - gera um token
  * Se não - Cria na DB, gera um token
  * Retornar o token com as infos do usuário logado
  */
 
+interface IAccessTokenResponse {
+  access_token: string;
+}
+
 export class AuthUserService {
   async execute(code: string) {
     const url = 'https://github.com/login/oauth/access_token';
-    const response = await axios.post(url, null, {
-      params: {
-        client_id: process.env.GITHUB_CLIENT_ID,
-        client_secret: process.env.GITHUB_CLIENT_SECRET,
-        code,
-      },
-      headers: {
-        Accept: 'application/json',
-      },
-    });
-    return response.data;
+    const { data: accessTokenResponse } =
+      await axios.post<IAccessTokenResponse>(url, null, {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+    return accessTokenResponse;
   }
 }
