d71a47dc30ef90f6d564208817e5dcb74b96ef66
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 19:37:44 -0300

    Controller 3 messages

---
+++ src/controller/GetLastThreeMessagesController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { GetLastThreeMessagesService } from '../services/GetLastThreeMessageService';
+
+export class GetLastThreeMessagesController {
+  async handle(req: Request, res: Response) {
+    const service = new GetLastThreeMessagesService();
+
+    const result = await service.execute();
+    return result;
+  }
+}
