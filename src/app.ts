7342c1f30d241180c25ea91ac7d7b680278ec52f
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 19:00:15 -0300

    Io event

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,23 @@
 import http from 'http';
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
+app.use(cors());
 
 const serverHttp = http.createServer(app);
-const io = new Server(serverHttp);
+const io = new Server(serverHttp, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado no socket ${socket}`);
+});
 
 app.use(express.json());
 app.use(router);
