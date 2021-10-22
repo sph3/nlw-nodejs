2f8088d06699f86ada21d32b2c5cb3e3774ef46d
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 18:21:19 -0300

    Add route message

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,13 @@
 import { Router } from 'express';
 import { AuthUserController } from './controller/AuthUserController';
+import { CreateMessageController } from './controller/CreateMessageController';
+import { ensureAuthenticated } from './middleware/ensureAuthenticated';
 
 export const router = Router();
 
 router.post('/auth', new AuthUserController().handle);
+router.post(
+  '/messages',
+  ensureAuthenticated,
+  new CreateMessageController().handle
+);
