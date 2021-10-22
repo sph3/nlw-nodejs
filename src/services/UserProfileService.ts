8675ebf2686d27b4e04a206c19fab4a5ff8f1b46
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 19:44:01 -0300

    User profile service

---
+++ src/services/UserProfileService.ts
@@ -0,0 +1,12 @@
+import { prismaClient } from '../prisma';
+
+export class UserProfileService {
+  async execute(user_id: string) {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        id: user_id,
+      },
+    });
+    return user;
+  }
+}
