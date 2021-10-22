d47ed0043b97dd785899e80d697ca33f371e5bf2
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 16:04:19 -0300

    Create route for Github authentication

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+import 'dotenv/config';
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+app.get('/github', (req, res) => {
+  res.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
+  );
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
