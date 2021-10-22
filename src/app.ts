3ecc95bbab612917c99938dd548e26e8376f5ec3
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 18:56:59 -0300

    Create server io

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { Server } from 'socket.io';
 import { router } from './routes';
 
 const app = express();
+
+const serverHttp = http.createServer(app);
+const io = new Server(serverHttp);
+
 app.use(express.json());
 app.use(router);
 
