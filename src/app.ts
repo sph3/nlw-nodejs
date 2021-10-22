a32ac8dd58d2193b203cb5e0c934aa83487e8127
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 17:14:10 -0300

    Fix

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import express from 'express';
 import { router } from './routes';
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 app.use(router);
 
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Oi');
+  res.send('Working!');
 });
 
 app.get('/github', (req, res) => {
