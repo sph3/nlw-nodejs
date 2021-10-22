0a204a2f5d56696182f18cd5c4f268f937a17b5e
juanhdev <juanhlealbarros@gmail.com>
2021-10-22 17:51:59 -0300

    Handle error

--- src/controller/AuthUserController.ts
+++ src/controller/AuthUserController.ts
@@ -5,7 +5,12 @@ export class AuthUserController {
   async handle(req: Request, res: Response) {
     const { code } = req.body;
     const service = new AuthUserService();
-    const result = await service.execute(code);
-    return res.json(result);
+
+    try {
+      const result = await service.execute(code);
+      return res.json(result);
+    } catch (err) {
+      return res.json(err.message);
+    }
   }
 }
